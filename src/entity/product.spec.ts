8b5ca62ae9e58a199ef5ea688040a9414f2e8649
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-06-03 14:05:05 -0300

    Criando classe Product

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,39 @@
+import Product from "./product";
+
+
+describe("Product unit tests", () => {
+
+    it("should throw error when id is empty", () => {
+
+        expect(() => {
+            const product = new Product("", "Product 1", 100);
+        }).toThrowError("Id is required");
+    });
+
+    it("should throw error when name is empty", () => {
+            
+            expect(() => {
+                const product = new Product("123", "", 100);
+            }).toThrowError("Name is required");
+        }
+    );
+
+    it("should throw error when price is less then 0", () => {
+                
+                expect(() => {
+                    const product = new Product("123", "Product 1", -100);
+                }).toThrowError("Price is required");
+    });
+
+    it("should change name", () => {
+        const product = new Product("123", "Product 1", 100);
+        product.changeName("Product 2");
+        expect(product.name).toBe("Product 2");
+    });
+
+    it("should change price", () => {
+        const product = new Product("123", "Product 1", 100);
+        product.changePrice(200);
+        expect(product.price).toBe(200);
+    });
+});
